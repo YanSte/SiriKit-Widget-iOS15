f2aa5cb19329b37203fe4828ba1d27ea2769d216
Yannick Stephan <stephan.yannick@me.com>
2021-10-04 09:05:15 +0100

    Initial Commit

---
+++ SiriKit-iOS15/SiriKit_iOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  SiriKit_iOS15App.swift
+//  SiriKit-iOS15
+//
+//  Created by Yannick Stephan on 04/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SiriKit_iOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
